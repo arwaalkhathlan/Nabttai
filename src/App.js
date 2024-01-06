a54d15754fe1255bed3f35f8fd470432670ec5dd
arwaalkhathlan <arwa124344@gmail.com>
2024-01-06 18:33:36 +0300

    Add Footer component to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Footer />
     </div>
   );
 }
