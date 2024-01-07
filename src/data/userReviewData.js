7cefc089e8a8603c97aca435a0bd89bc8ba77e97
arwaalkhathlan <arwa124344@gmail.com>
2024-01-07 03:47:34 +0300

    Update UserReviewCard component and userReviewData
    This commit updates the UserReviewCard component by replacing the star emoji with an SVG icon. It also updates the userReviewData file with Arabic text for the reviewText and userName fields.

--- src/data/userReviewData.js
+++ src/data/userReviewData.js
@@ -3,9 +3,9 @@ import user_2 from '../images/user_2.jpeg';
 import user_3 from '../images/user_3.jpeg';
 
 const userReviewData = [
-  { id: 1, userProfilePicture: user_1, reviewText: 'Great experience with the product.', userName: 'John Doe', starRating: 4 },
-  { id: 2, userProfilePicture: user_2, reviewText: 'Amazing service and fast delivery.', userName: 'Jane Smith', starRating: 5 },
-  { id: 3, userProfilePicture: user_3, reviewText: 'Highly recommended. Will buy again.', userName: 'Bob Johnson', starRating: 3 },
+  { id: 1, userProfilePicture: user_1, reviewText: 'النبتة رائعة وتضيف لمسة من الطبيعة إلى المنزل. يبدو أن الرعاية الجيدة تعزز نموها، وأنا ممتن للهدية الخضراء هذه. الأوراق الخضراء الزاهية تجعل البيئة أكثر إشراقًا', userName: 'ريم محمد حسن', starRating: 4 },
+  { id: 2, userProfilePicture: user_2, reviewText: 'اقتنيت هذه النبتة لتحسين جودة الهواء في الغرفة، وهي تقوم بالمهمة بشكل رائع. العناية بها سهلة، وتحافظ على رطوبة الهواء. أحب الهدوء الذي تجلبه إلى الديكور.', userName: 'لمى عبد الرحمن علي', starRating: 5 },
+  { id: 3, userProfilePicture: user_3, reviewText: 'نبتة جميلة ومنظر مريح للعيون. تعتبر هذه النبتة إضافة مثالية لزاوية القراءة في المنزل. تساعدني على الاسترخاء وتحسين البيئة المحيطة. أنصح بها لمحبي النباتات الداخلية.', userName: 'آمنة جمال مصطفى', starRating: 3 },
 ];
 
 export default userReviewData;
