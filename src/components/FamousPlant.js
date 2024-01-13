3dbfe28d73569413fb18f06c08c3967aec2c64a1
Alizelabd <lordlords834@gmail.com>
2024-01-13 23:02:29 +0300

    endou recent update

--- src/components/FamousPlant.js
+++ src/components/FamousPlant.js
@@ -22,7 +22,7 @@ function FamousPlant() {
                     <div className="slid-div box-shadow d-inline-flex" >
                         <div className="image-plant"><img src={FeeImg} alt="Fee" /></div>
                         <div className="info">
-                            <h5 className='text-shadow'>تعرف أكثر على فيّ</h5>
+                            <h5 className='text-shadow'>فيّ</h5>
                             <p className='text-shadow'>
                                 جمال الصمود تأخذنا في رحلة مشوقة إلى عالمٍ ساحر، حيث تتعايش الطبيعة الخلابة مع روح الإصرار والأمل. تستعرض القصة قوة الصمود والتأقلم في وجه التحديات، وتجسد قوة العلاقات الإنسانية في تحقيق التغيير والنمو
                             </p>
@@ -35,7 +35,7 @@ function FamousPlant() {
                     <div className="slid-div box-shadow d-inline-flex" >
                         <div className="image-plant"><img src={sabaraImg} alt="sabara" /></div>
                         <div className="info">
-                            <h5 className='text-shadow'>تعرف أكثر على صبرا</h5>
+                            <h5 className='text-shadow'>صبرا</h5>
                             <p className='text-shadow'>في قريةٍ بعيدة، تنمو نبتةٌ ساحرة تُدعى صبرا. بجمالها الخلاب وقوتها الداخلية، تلهم الناس وتُذَكِّرهم بأهمية الصمود والأمل. تتحول صبرا إلى رمزٍ للقوة والتكيف، وسرعان ما تنتشر شهرتها في الأرجاء. وسط الصعاب والتحديات، هل ستواصل صبرا تألقها وتحقيق الجمال الذي تعد به؟</p>
                             <Link to={`/PlantsStories/1`} className="btn green-btn" > للمزيد </Link>
                         </div>
@@ -45,7 +45,7 @@ function FamousPlant() {
                     <div className="slid-div box-shadow d-inline-flex" >
                         <div className="image-plant"><img src={nseemImg} alt="nseem" /></div>
                         <div className="info">
-                            <h5 className='text-shadow'>تعرف أكثر على نسيم</h5>
+                            <h5 className='text-shadow'>نسيم</h5>
                             <p className='text-shadow'>في جنبات الغابة، اكتشف يوسف نَسيم، النبتة الصغيرة التي غيرت حياته، فما هي القصة المدهشة وراء هذا الاكتشاف؟</p>
                             <Link to={`/PlantsStories/3`} className="btn green-btn" > للمزيد </Link>
                         </div>
