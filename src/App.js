655126c380681acacf331f373b472f913e5c4283
azam <azam.alkhodiriy@gmail.com>
2024-01-22 02:46:20 +0300

    Add react-ga package and initialize Google Analytics tracking

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import React, { useEffect } from 'react';
 import Home from './Pages/Home';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import OurProducts from './Pages/OurProducts';
 import AboutUs from './Pages/AboutUs';
 import ProductsPage from './Pages/ProductsPage';
 import PlantsStories from './Pages/PlantsStories';
-// import FormsBox from './components/FormsBox';
+import ReactGA from 'react-ga';
+
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize('YOUR_TRACKING_ID'); // Replace with your Google Analytics tracking ID
+  }, []);
+
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+
   return (
     <div className="App">
-            {/* <FormsBox /> */}
+      {/* <FormsBox /> */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/OurProducts' element={<OurProducts />} />
